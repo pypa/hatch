e46b7c0d8e2e2dade54df3cf405342ca76bd21a4
ofek <ofekmeister@gmail.com>
2017-06-20 23:01:02 -0400

    test git

---
+++ tests/vc/test_git.py
@@ -0,0 +1,28 @@
+import os
+
+from hatch.core import create_package
+from hatch.settings import DEFAULT_SETTINGS
+from ..utils import read_file, temp_chdir
+
+
+def test_setup():
+    with temp_chdir() as d:
+        settings = DEFAULT_SETTINGS.copy()
+        settings['vc'] = 'git'
+        create_package(d, 'ok', settings)
+
+        assert os.path.exists(os.path.join(d, '.git'))
+        assert os.path.exists(os.path.join(d, '.gitattributes'))
+        assert os.path.exists(os.path.join(d, '.gitignore'))
+
+
+def test_gitattributes():
+    with temp_chdir() as d:
+        settings = DEFAULT_SETTINGS.copy()
+        settings['vc'] = 'git'
+        create_package(d, 'ok', settings)
+
+        assert read_file(os.path.join(d, '.gitattributes')) == (
+            '# Auto detect text files and perform LF normalization\n'
+            '* text=auto\n'
+        )
