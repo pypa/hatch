1939d9c2a4268f4edf2daa2642bb60ce8f7d56ea
ofek <ofekmeister@gmail.com>
2017-07-31 22:19:24 -0400

    improve coverage

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,7 +1,8 @@
 import os
 
 from hatch.utils import (
-    chdir, create_file, get_current_year, normalize_package_name, temp_chdir
+    chdir, create_file, get_current_year, normalize_package_name, temp_chdir,
+    temp_move_path
 )
 
 
@@ -29,3 +30,23 @@ def test_temp_chdir():
         create_file(os.path.join(d, 'file.txt'))
 
     assert not os.path.exists(d)
+
+
+def test_temp_move_path():
+    with temp_chdir() as d1:
+        path = os.path.join(d1, 'test')
+        create_file(path)
+        assert os.path.exists(path)
+
+        with temp_chdir() as d2:
+            with temp_move_path(path, d2) as dst:
+                assert not os.path.exists(path)
+                assert dst == os.path.join(d2, 'test')
+
+        assert os.path.exists(path)
+
+
+def test_temp_move_path_not_exist():
+    with temp_chdir() as d:
+        with temp_move_path(os.path.join(d, 'test'), d) as dst:
+            pass
