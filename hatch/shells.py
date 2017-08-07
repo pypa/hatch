332d5f377ce8c4e6d038981d9cee529a995cd5f2
ofek <ofekmeister@gmail.com>
2017-08-07 17:55:14 -0400

    try things

--- hatch/shells.py
+++ hatch/shells.py
@@ -3,7 +3,7 @@ from hatch.utils import NEED_SUBPROCESS_SHELL
 DEFAULT_SHELL = 'cmd' if NEED_SUBPROCESS_SHELL else 'bash'
 
 SHELL_COMMANDS = {
-    'cmd': 'cmd /k',
+    'cmd': ['cmd', '/k'],
     'bash': 'bash --init-file <(echo -e "source ~/.bashrc\nPS1=\"(ok) $PS1\"")',
-    'zsh': 'zsh',
+    'zsh': ['zsh'],
 }
