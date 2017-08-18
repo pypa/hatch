5ae1e0feb93ffce0ddb904a1dbcc8b071a9c4f55
ofek <ofekmeister@gmail.com>
2017-08-17 23:00:11 -0400

    move

--- tests/test_venv.py
+++ tests/test_venv.py
@@ -10,6 +10,68 @@ from hatch.venv import create_venv, fix_executable, venv
 from .utils import read_file
 
 
+def test_default():
+    with temp_chdir() as d:
+        d = os.path.join(d, 'test_env')
+        create_venv(d)
+
+        assert os.path.exists(d)
+
+
+def test_pypath():
+    with temp_chdir() as d:
+        d = os.path.join(d, 'test_env')
+        create_venv(d, pypath=get_python_path())
+
+        assert os.path.exists(d)
+
+
+def test_venv():
+    with temp_chdir() as d:
+        d = os.path.join(d, 'test_env')
+        create_venv(d)
+        global_python = get_python_path()
+
+        with venv(d):
+            venv_python = get_python_path()
+
+        assert global_python != venv_python
+        assert global_python == get_python_path()
+
+
+def test_venv_unknown():
+    with temp_chdir() as d:
+        d = os.path.join(d, 'test_env')
+        create_venv(d)
+
+        with pytest.raises(OSError):
+            if os.path.exists(os.path.join(d, 'bin')):  # no cov
+                shutil.rmtree(os.path.join(d, 'bin'))
+            if os.path.exists(os.path.join(d, 'Scripts')):  # no cov
+                shutil.rmtree(os.path.join(d, 'Scripts'))
+            with venv(d):  # no cov
+                pass
+
+
+def test_levels():
+    with temp_chdir() as d:
+        d1 = os.path.join(d, 'test_env1')
+        d2 = os.path.join(d, 'test_env2')
+        d3 = os.path.join(d, 'test_env3')
+        create_venv(d1)
+        create_venv(d2)
+        create_venv(d3)
+
+        with venv(d1):
+            assert os.environ.get('_HATCH_LEVEL_') == '1'
+            with venv(d2):
+                assert os.environ.get('_HATCH_LEVEL_') == '2'
+                with venv(d3):
+                    assert os.environ.get('_HATCH_LEVEL_') == '3'
+                assert os.environ.get('_HATCH_LEVEL_') == '2'
+            assert os.environ.get('_HATCH_LEVEL_') == '1'
+
+
 class TestFixExecutable:
     def test_no_hashbang(self):
         with temp_chdir() as d:
@@ -173,65 +235,3 @@ class TestFixExecutable:
                 "    sys.argv[0] = re.sub(r'(-script\.pyw?|\.exe)?$', '', sys.argv[0])\n"
                 '    sys.exit(main())\n'.format(new_path + os.path.sep)
             )
-
-
-def test_default():
-    with temp_chdir() as d:
-        d = os.path.join(d, 'test_env')
-        create_venv(d)
-
-        assert os.path.exists(d)
-
-
-def test_pypath():
-    with temp_chdir() as d:
-        d = os.path.join(d, 'test_env')
-        create_venv(d, pypath=get_python_path())
-
-        assert os.path.exists(d)
-
-
-def test_venv():
-    with temp_chdir() as d:
-        d = os.path.join(d, 'test_env')
-        create_venv(d)
-        global_python = get_python_path()
-
-        with venv(d):
-            venv_python = get_python_path()
-
-        assert global_python != venv_python
-        assert global_python == get_python_path()
-
-
-def test_venv_unknown():
-    with temp_chdir() as d:
-        d = os.path.join(d, 'test_env')
-        create_venv(d)
-
-        with pytest.raises(OSError):
-            if os.path.exists(os.path.join(d, 'bin')):  # no cov
-                shutil.rmtree(os.path.join(d, 'bin'))
-            if os.path.exists(os.path.join(d, 'Scripts')):  # no cov
-                shutil.rmtree(os.path.join(d, 'Scripts'))
-            with venv(d):  # no cov
-                pass
-
-
-def test_levels():
-    with temp_chdir() as d:
-        d1 = os.path.join(d, 'test_env1')
-        d2 = os.path.join(d, 'test_env2')
-        d3 = os.path.join(d, 'test_env3')
-        create_venv(d1)
-        create_venv(d2)
-        create_venv(d3)
-
-        with venv(d1):
-            assert os.environ.get('_HATCH_LEVEL_') == '1'
-            with venv(d2):
-                assert os.environ.get('_HATCH_LEVEL_') == '2'
-                with venv(d3):
-                    assert os.environ.get('_HATCH_LEVEL_') == '3'
-                assert os.environ.get('_HATCH_LEVEL_') == '2'
-            assert os.environ.get('_HATCH_LEVEL_') == '1'
