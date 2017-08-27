31b9c6701d3130cd13f8d8c063bb799e949a6637
ofek <ofekmeister@gmail.com>
2017-08-27 18:57:49 -0400

    fix build

--- hatch/build.py
+++ hatch/build.py
@@ -5,17 +5,22 @@ from hatch.utils import NEED_SUBPROCESS_SHELL, chdir, get_proper_python
 
 def build_package(d, universal=None, name=None, build_dir=None):
     command = [
-        get_proper_python(), 'setup.py', 'sdist', 'bdist_wheel'
+        get_proper_python(), 'setup.py', 'sdist'
     ]
+
+    if build_dir:
+        command.extend(['--dist-dir', build_dir])
+
+    command.append('bdist_wheel')
+
+    if build_dir:
+        command.extend(['--dist-dir', build_dir])
 
     if universal:
         command.append('--universal')
 
     if name:
         command.extend(['--plat-name', name])
-
-    if build_dir:
-        command.extend(['--dist-dir', build_dir])
 
     with chdir(d):
         result = subprocess.run(command, shell=NEED_SUBPROCESS_SHELL)
