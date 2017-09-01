4e10737c5afae5a698874f196458a96eb05e66f6
ofek <ofekmeister@gmail.com>
2017-09-01 16:08:35 -0400

    more tests

--- tests/commands/test_shed.py
+++ tests/commands/test_shed.py
@@ -45,6 +45,23 @@ def test_pyname():
         assert 'Successfully removed Python path named `pyname`.' in result.output
 
 
+def test_pyname_multiple():
+    with temp_chdir() as d:
+        runner = CliRunner()
+
+        with temp_move_path(SETTINGS_FILE, d):
+            settings = copy_default_settings()
+            settings['pythons']['pyname1'] = 'pypath1'
+            settings['pythons']['pyname2'] = 'pypath2'
+            save_settings(settings)
+            result = runner.invoke(hatch, ['shed', '-p', 'pyname1,pyname2'])
+            assert load_settings()['pythons'] == {}
+
+        assert result.exit_code == 0
+        assert 'Successfully removed Python path named `pyname1`.' in result.output
+        assert 'Successfully removed Python path named `pyname2`.' in result.output
+
+
 def test_pyname_not_exist():
     with temp_chdir() as d:
         runner = CliRunner()
@@ -77,6 +94,41 @@ def test_env():
 
         assert result.exit_code == 0
         assert 'Successfully removed virtual env named `{}`.'.format(env_name) in result.output
+
+
+def test_env_multiple():
+    with temp_chdir():
+        runner = CliRunner()
+
+        env_name1 = os.urandom(10).hex()
+        while os.path.exists(os.path.join(VENV_DIR, env_name1)):  # no cov
+            env_name1 = os.urandom(10).hex()
+
+        venv_dir1 = os.path.join(VENV_DIR, env_name1)
+        env_name2 = ''
+        venv_dir2 = ''
+
+        try:
+            runner.invoke(hatch, ['env', env_name1])
+            assert os.path.exists(venv_dir1)
+
+            env_name2 = os.urandom(10).hex()
+            while os.path.exists(os.path.join(VENV_DIR, env_name2)):  # no cov
+                env_name2 = os.urandom(10).hex()
+            venv_dir2 = os.path.join(VENV_DIR, env_name2)
+            runner.invoke(hatch, ['env', env_name2])
+            assert os.path.exists(venv_dir2)
+
+            result = runner.invoke(hatch, ['shed', '-e', '{},{}'.format(env_name1, env_name2)])
+            assert not os.path.exists(venv_dir1)
+            assert not os.path.exists(venv_dir2)
+        finally:
+            remove_path(venv_dir1)
+            remove_path(venv_dir2)
+
+        assert result.exit_code == 0
+        assert 'Successfully removed virtual env named `{}`.'.format(env_name1) in result.output
+        assert 'Successfully removed virtual env named `{}`.'.format(env_name2) in result.output
 
 
 def test_env_not_exist():
