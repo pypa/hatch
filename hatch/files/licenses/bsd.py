04f5c004b5a4606268728de71ad197042e4015fc
Amjith Ramanujam <amjith.r@gmail.com>
2017-09-23 12:32:21 -0700

    Allow BSDLicense import and change to bsd-3.

--- hatch/files/licenses/bsd.py
+++ hatch/files/licenses/bsd.py
@@ -2,7 +2,7 @@ from hatch.structures import File
 from hatch.utils import get_current_year
 
 TEMPLATE = """\
-BSD 2-Clause License
+BSD 3-Clause License
 
 Copyright (c) {year}, {author}
 All rights reserved.
@@ -16,6 +16,10 @@ modification, are permitted provided that the following conditions are met:
 * Redistributions in binary form must reproduce the above copyright notice,
   this list of conditions and the following disclaimer in the documentation
   and/or other materials provided with the distribution.
+
+* Neither the name of the copyright holder nor the names of its
+  contributors may be used to endorse or promote products derived from
+  this software without specific prior written permission.
 
 THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
 AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
@@ -39,4 +43,4 @@ class BSDLicense(File):
         self.short_name = 'BSD'
         self.long_name = 'BSD License'
         self.pypi_classifier = 'License :: OSI Approved :: BSD License'
-        self.url = 'https://choosealicense.com/licenses/bsd'
+        self.url = 'https://choosealicense.com/licenses/bsd-3-clause'
