8406aa4613e1a9067340e6e36b840804b0da655b
ofek <ofekmeister@gmail.com>
2017-10-10 02:25:45 -0400

    no need

--- hatch/conda.py
+++ hatch/conda.py
@@ -1,13 +1,12 @@
 import os
 
 from hatch.utils import ON_WINDOWS
-from hatch.venv import locate_exe_dir
 
 
 def get_conda_new_exe_path(path):  # no cov
     if ON_WINDOWS:
         return os.pathsep.join((
-            path, locate_exe_dir(path, check=False), os.path.join(path, 'Library', 'bin')
+            path, os.path.join(path, 'Scripts'), os.path.join(path, 'Library', 'bin')
         ))
     else:
-        return locate_exe_dir(path, check=False)
+        return os.path.join(path, 'bin')
