0039f17ca818fb432385c148e757098b79f963e7
Ofek Lev <ofekmeister@gmail.com>
2022-03-06 12:06:52 -0500

    Add full tests for the `dep show table` command

--- src/hatch/cli/dep/__init__.py
+++ src/hatch/cli/dep/__init__.py
@@ -1,21 +1,6 @@
 import click
 
 
-def get_unique_dependencies(all_dependencies):
-    dependencies = set()
-    for dependency in all_dependencies:
-        # Remove any surrounding white space
-        dependency = dependency.strip()
-        # Internal spacing is ignored by PEP 440
-        dependency = dependency.replace(' ', '')
-        # Normalize casing
-        dependency = dependency.lower()
-
-        dependencies.add(dependency)
-
-    return dependencies
-
-
 @click.group(short_help='Manage environment dependencies')
 def dep():
     pass
@@ -29,6 +14,8 @@ def hash_dependencies(app, project_only, env_only):
     """Output a hash of the currently defined dependencies."""
     from hashlib import sha256
 
+    from ...utils.dep import get_normalized_dependencies
+
     all_dependencies = []
     if project_only:
         all_dependencies.extend(app.project.metadata.core.dependencies)
@@ -40,7 +27,13 @@ def hash_dependencies(app, project_only, env_only):
         environment = app.get_environment()
         all_dependencies.extend(environment.environment_dependencies)
 
-    data = ''.join(sorted(get_unique_dependencies(all_dependencies))).encode('utf-8')
+    data = ''.join(
+        sorted(
+            # Internal spacing is ignored by PEP 440
+            dependency.replace(' ', '')
+            for dependency in get_normalized_dependencies(all_dependencies)
+        )
+    ).encode('utf-8')
 
     app.display_info(sha256(data).hexdigest())
 
@@ -54,11 +47,13 @@ def show():
 @click.option('--project-only', '-p', is_flag=True, help='Whether or not to exclude environment dependencies')
 @click.option('--env-only', '-e', is_flag=True, help='Whether or not to exclude project dependencies')
 @click.option('--lines', '-l', 'show_lines', is_flag=True, help='Whether or not to show lines between table rows')
+@click.option('--ascii', 'force_ascii', is_flag=True, help='Whether or not to only use ASCII characters')
 @click.pass_obj
-def table(app, project_only, env_only, show_lines):
+def table(app, project_only, env_only, show_lines, force_ascii):
     """Enumerate dependencies in a tabular format."""
     from packaging.requirements import Requirement
-    from rich.table import Table
+
+    from ...utils.dep import get_normalized_dependencies
 
     project_dependencies = []
     environment_dependencies = []
@@ -73,47 +68,36 @@ def table(app, project_only, env_only, show_lines):
         environment = app.get_environment()
         environment_dependencies.extend(environment.environment_dependencies)
 
-    for dependencies, title in (
+    for dependencies, table_title in (
         (project_dependencies, 'Project'),
-        (environment_dependencies, f'Environment: {app.env}'),
+        (environment_dependencies, f'Env: {app.env}'),
     ):
         if not dependencies:
             continue
 
-        dependency_table = Table(title=title, show_lines=show_lines)
+        requirements = [Requirement(d) for d in get_normalized_dependencies(dependencies)]
 
-        # There will always be a name
-        dependency_table.add_column('Name', style='bold', no_wrap=True)
+        columns = {'Name': {}, 'URL': {}, 'Versions': {}, 'Markers': {}, 'Features': {}}
+        for i, requirement in enumerate(requirements):
+            columns['Name'][i] = requirement.name
 
-        requirements = sorted((Requirement(d) for d in get_unique_dependencies(dependencies)), key=lambda r: r.name)
+            if requirement.url:
+                columns['URL'][i] = str(requirement.url)
 
-        potential_attributes = {'url': False, 'specifier': False, 'marker': False, 'extras': False}
-        attribute_titles = {'url': 'URL', 'specifier': 'Versions', 'marker': 'Markers', 'extras': 'Features'}
+            if requirement.specifier:
+                columns['Versions'][i] = str(requirement.specifier)
 
-        for requirement in requirements:
-            for attribute in potential_attributes:
-                if getattr(requirement, attribute):
-                    potential_attributes[attribute] = True
+            if requirement.marker:
+                columns['Markers'][i] = str(requirement.marker)
 
-        present_attributes = []
-        for attribute, present in potential_attributes.items():
-            if present:
-                present_attributes.append(attribute)
-                dependency_table.add_column(attribute_titles[attribute], style='bold', no_wrap=attribute != 'url')
+            if requirement.extras:
+                columns['Features'][i] = ', '.join(sorted(requirement.extras))
 
-        for requirement in requirements:
-            attributes = [requirement.name]
-            for attribute in present_attributes:
-                value = getattr(requirement, attribute)
-                if not value:
-                    value = ''
-                elif attribute == 'extras':
-                    value = ', '.join(sorted(value))
-                else:
-                    value = str(value)
+        column_options = {}
+        for column_title in columns:
+            if column_title != 'URL':
+                column_options[column_title] = {'no_wrap': True}
 
-                attributes.append(value)
-
-            dependency_table.add_row(*attributes)
-
-        app.display(dependency_table)
+        app.display_table(
+            table_title, columns, show_lines=show_lines, column_options=column_options, force_ascii=force_ascii
+        )
---
+++ tests/cli/dep/show/test_table.py
@@ -0,0 +1,180 @@
+import pytest
+
+from hatch.project.core import Project
+from hatch.utils.structures import EnvVars
+
+
+@pytest.fixture(scope='module', autouse=True)
+def terminal_width():
+    with EnvVars({'COLUMNS': '200'}):
+        yield
+
+
+def test_project_only(hatch, helpers, temp_dir, config_file):
+    config_file.model.template.plugins['default']['tests'] = False
+    config_file.save()
+
+    project_name = 'My App'
+
+    with temp_dir.as_cwd():
+        result = hatch('new', project_name)
+
+    assert result.exit_code == 0, result.output
+
+    project_path = temp_dir / 'my-app'
+    data_path = temp_dir / 'data'
+    data_path.mkdir()
+
+    project = Project(project_path)
+    config = dict(project.raw_config)
+    config['project']['dependencies'] = ['foo-bar-baz']
+    project.save_config(config)
+
+    with project_path.as_cwd():
+        result = hatch('dep', 'show', 'table', '--ascii', '-p')
+
+    assert result.exit_code == 0, result.output
+    assert helpers.remove_trailing_spaces(result.output) == helpers.dedent(
+        """
+            Project
+        +-------------+
+        | Name        |
+        +=============+
+        | foo-bar-baz |
+        +-------------+
+        """
+    )
+
+
+def test_environment_only(hatch, helpers, temp_dir, config_file):
+    config_file.model.template.plugins['default']['tests'] = False
+    config_file.save()
+
+    project_name = 'My App'
+
+    with temp_dir.as_cwd():
+        result = hatch('new', project_name)
+
+    assert result.exit_code == 0, result.output
+
+    project_path = temp_dir / 'my-app'
+    data_path = temp_dir / 'data'
+    data_path.mkdir()
+
+    project = Project(project_path)
+    helpers.update_project_environment(project, 'default', {'dependencies': ['foo-bar-baz']})
+
+    with project_path.as_cwd():
+        result = hatch('dep', 'show', 'table', '--ascii', '-e')
+
+    assert result.exit_code == 0, result.output
+    assert helpers.remove_trailing_spaces(result.output) == helpers.dedent(
+        """
+         Env: default
+        +-------------+
+        | Name        |
+        +=============+
+        | foo-bar-baz |
+        +-------------+
+        """
+    )
+
+
+def test_default_both(hatch, helpers, temp_dir, config_file):
+    config_file.model.template.plugins['default']['tests'] = False
+    config_file.save()
+
+    project_name = 'My App'
+
+    with temp_dir.as_cwd():
+        result = hatch('new', project_name)
+
+    assert result.exit_code == 0, result.output
+
+    project_path = temp_dir / 'my-app'
+    data_path = temp_dir / 'data'
+    data_path.mkdir()
+
+    project = Project(project_path)
+    config = dict(project.raw_config)
+    config['project']['dependencies'] = ['foo-bar-baz']
+    project.save_config(config)
+    helpers.update_project_environment(project, 'default', {'dependencies': ['baz-bar-foo']})
+
+    with project_path.as_cwd():
+        result = hatch('dep', 'show', 'table', '--ascii')
+
+    assert result.exit_code == 0, result.output
+    assert helpers.remove_trailing_spaces(result.output) == helpers.dedent(
+        """
+            Project
+        +-------------+
+        | Name        |
+        +=============+
+        | foo-bar-baz |
+        +-------------+
+         Env: default
+        +-------------+
+        | Name        |
+        +=============+
+        | baz-bar-foo |
+        +-------------+
+        """
+    )
+
+
+def test_optional_columns(hatch, helpers, temp_dir, config_file):
+    config_file.model.template.plugins['default']['tests'] = False
+    config_file.save()
+
+    project_name = 'My App'
+
+    with temp_dir.as_cwd():
+        result = hatch('new', project_name)
+
+    assert result.exit_code == 0, result.output
+
+    project_path = temp_dir / 'my-app'
+    data_path = temp_dir / 'data'
+    data_path.mkdir()
+
+    project = Project(project_path)
+    config = dict(project.raw_config)
+    config['project']['dependencies'] = [
+        'python___dateutil', 'bAr.Baz[TLS, EdDSA]   >=1.2RC5', 'Foo;python_version<"3.8"'
+    ]
+    project.save_config(config)
+    helpers.update_project_environment(
+        project,
+        'default',
+        {
+            'dependencies': [
+                'proj @ git+https://github.com/org/proj.git@v1',
+                'bAr.Baz   [TLS, EdDSA]   >=1.2RC5;python_version<"3.8"',
+            ],
+        },
+    )
+
+    with project_path.as_cwd():
+        result = hatch('dep', 'show', 'table', '--ascii')
+
+    assert result.exit_code == 0, result.output
+    assert helpers.remove_trailing_spaces(result.output) == helpers.dedent(
+        """
+                                      Project
+        +-----------------+----------+------------------------+------------+
+        | Name            | Versions | Markers                | Features   |
+        +=================+==========+========================+============+
+        | bar-baz         | >=1.2rc5 |                        | eddsa, tls |
+        | foo             |          | python_version < "3.8" |            |
+        | python-dateutil |          |                        |            |
+        +-----------------+----------+------------------------+------------+
+                                                    Env: default
+        +---------+----------------------------------------+----------+------------------------+------------+
+        | Name    | URL                                    | Versions | Markers                | Features   |
+        +=========+========================================+==========+========================+============+
+        | bar-baz |                                        | >=1.2rc5 | python_version < "3.8" | eddsa, tls |
+        | proj    | git+https://github.com/org/proj.git@v1 |          |                        |            |
+        +---------+----------------------------------------+----------+------------------------+------------+
+        """
+    )
