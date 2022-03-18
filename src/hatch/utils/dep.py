8d633c0c5ba6ca9e417283b20df97b3207a89f73
Ofek Lev <ofekmeister@gmail.com>
2022-03-18 00:55:28 -0400

    reuse util

--- src/hatch/utils/dep.py
+++ src/hatch/utils/dep.py
@@ -1,6 +1,6 @@
-import re
-
 from packaging.requirements import Requirement
+
+from hatchling.metadata.utils import get_normalized_dependency
 
 
 def normalize_marker_quoting(text):
@@ -9,13 +9,5 @@ def normalize_marker_quoting(text):
 
 
 def get_normalized_dependencies(dependencies):
-    normalized_dependencies = set()
-    for dependency in dependencies:
-        requirement = Requirement(dependency)
-
-        # https://www.python.org/dev/peps/pep-0503/#normalized-names
-        requirement.name = re.sub(r'[-_.]+', '-', requirement.name)
-
-        normalized_dependencies.add(normalize_marker_quoting(str(requirement).lower()))
-
+    normalized_dependencies = {get_normalized_dependency(Requirement(dependency)) for dependency in dependencies}
     return sorted(normalized_dependencies)
