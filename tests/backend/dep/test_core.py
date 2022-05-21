24fe4d1f4e6e429dcdd0a8bdd019b1d3eb4a240f
Ben Beasley <code@musicinmybrain.net>
2022-05-20 21:26:43 -0400

    Add the `requires_internet` mark to a few more tests (#253)
    * Add the “requires_internet” mark to a few more tests
    
    * fix style
    
    Co-authored-by: Ofek Lev <ofekmeister@gmail.com>

--- tests/backend/dep/test_core.py
+++ tests/backend/dep/test_core.py
@@ -1,5 +1,6 @@
 import sys
 
+import pytest
 from packaging.requirements import Requirement
 
 from hatch.venv.core import TempVirtualEnv
@@ -16,12 +17,14 @@ def test_dependency_not_found(platform):
         assert not dependencies_in_sync([Requirement('binary')], venv.sys_path)
 
 
+@pytest.mark.requires_internet
 def test_dependency_found(platform):
     with TempVirtualEnv(sys.executable, platform) as venv:
         platform.run_command(['pip', 'install', 'binary'], check=True, capture_output=True)
         assert dependencies_in_sync([Requirement('binary')], venv.sys_path)
 
 
+@pytest.mark.requires_internet
 def test_version_unmet(platform):
     with TempVirtualEnv(sys.executable, platform) as venv:
         platform.run_command(['pip', 'install', 'binary'], check=True, capture_output=True)
@@ -38,24 +41,28 @@ def test_marker_unmet(platform):
         assert not dependencies_in_sync([Requirement('binary; python_version > "1"')], venv.sys_path)
 
 
+@pytest.mark.requires_internet
 def test_extra_no_dependencies(platform):
     with TempVirtualEnv(sys.executable, platform) as venv:
         platform.run_command(['pip', 'install', 'binary'], check=True, capture_output=True)
         assert not dependencies_in_sync([Requirement('binary[foo]')], venv.sys_path)
 
 
+@pytest.mark.requires_internet
 def test_unknown_extra(platform):
     with TempVirtualEnv(sys.executable, platform) as venv:
         platform.run_command(['pip', 'install', 'requests[security]==2.25.1'], check=True, capture_output=True)
         assert not dependencies_in_sync([Requirement('requests[foo]')], venv.sys_path)
 
 
+@pytest.mark.requires_internet
 def test_extra_unmet(platform):
     with TempVirtualEnv(sys.executable, platform) as venv:
         platform.run_command(['pip', 'install', 'requests==2.25.1'], check=True, capture_output=True)
         assert not dependencies_in_sync([Requirement('requests[security]==2.25.1')], venv.sys_path)
 
 
+@pytest.mark.requires_internet
 def test_extra_met(platform):
     with TempVirtualEnv(sys.executable, platform) as venv:
         platform.run_command(['pip', 'install', 'requests[security]==2.25.1'], check=True, capture_output=True)
