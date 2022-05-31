f1dd18f99e334b56da4318d0bc7c8f323df72439
Ofek Lev <ofekmeister@gmail.com>
2022-05-30 21:33:57 -0400

    Apply suggestions from code review

--- src/hatch/cli/new/migrate.py
+++ src/hatch/cli/new/migrate.py
@@ -235,7 +235,7 @@ def setup(**kwargs):
             for relative_directory, files in sorted(relative_files.items()):
                 if not os.path.isdir(relative_directory) or set(os.listdir(relative_directory)) != set(files):
                     for filename in sorted(files):
-                        local_path = os.path.join(relative_directory, filename)
+                        local_path = os.path.join(relative_directory, filename).replace('\\', '/')
                         shared_data[local_path] = f'{shared_directory}/{filename}'
                 else:
                     shared_data[relative_directory] = shared_directory
