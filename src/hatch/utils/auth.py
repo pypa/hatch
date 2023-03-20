24ce4c553674a4ac195ed6eb68302dfd2df35f0a
Frank Sachsenheim <funkyfuture@riseup.net>
2023-03-20 21:59:24 +0100

    publish: Implements credentials retrieval from .pypirc

--- src/hatch/utils/auth.py
+++ src/hatch/utils/auth.py
@@ -1,3 +1,11 @@
+# TODO complete type annotations
+# TODO tests
+# TODO docs
+
+from __future__ import annotations
+
+
+import configparser
 import json
 
 from hatch.utils.fs import Path
@@ -8,6 +16,10 @@ def get_auth(app, username, options, repo, repo_config) -> tuple[bool, str]:
         return False, options['auth']
 
     auth_token = repo_config.get('auth', '')
+    if auth_token:
+        return False, auth_token
+
+    _, auth_token = lookup_pypirc(repo, repo_config)
     if auth_token:
         return False, auth_token
 
@@ -31,6 +43,10 @@ def get_user(app, cached_user_file, options, repo, repo_config) -> tuple[bool, s
     if username:
         return False, username
 
+    username, _ = lookup_pypirc(repo, repo_config)
+    if username:
+        return False, username
+
     username = cached_user_file.get_user(repo)
     if username is not None:
         return False, username
@@ -41,10 +57,35 @@ def get_user(app, cached_user_file, options, repo, repo_config) -> tuple[bool, s
     return True, app.prompt('Enter your username')
 
 
+def lookup_pypirc(repo, repo_config) -> tuple[None | str, None | str]:
+    config = configparser.ConfigParser()
+    config.read(Path.home() / ".pypirc")
+
+    if not repo:
+        repo = "pypi"
+
+    if config.has_section(repo):
+        return (
+            config.get(section=repo, option="username", fallback=None),
+            config.get(section=repo, option="password", fallback=None)
+        )
+
+    repo_url = repo_config["url"]
+    assert isinstance(repo_url, str)
+    for section in config.sections():
+        if config.get(section=section, option="repository", fallback=None) == repo_url:
+            return (
+                config.get(section=section, option="username", fallback=None),
+                config.get(section=section, option="password", fallback=None)
+            )
+
+    return None, None
+
+
 class CachedUserFile:
     def __init__(self, cache_dir: Path):
         self.path = cache_dir / 'previous_working_users.json'
-        self._data = None
+        self._data: Optional[dict[str, str]] = None
 
     def get_user(self, repo: str):
         return self.data.get(repo)
