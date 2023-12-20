a486beaedf01d572ea36448a9aa4da14a8d00a48
juftin <juftin@juftin.com>
2023-12-19 20:31:14 -0700

    fix: updated dependency hash

--- src/hatch/cli/application.py
+++ src/hatch/cli/application.py
@@ -114,7 +114,8 @@ class Application(Terminal):
                 with self.status('Syncing dependencies'):
                     environment.sync_dependencies()
 
-            self.env_metadata.update_dependency_hash(environment, dep_hash)
+            new_dep_hash = environment.dependency_hash()
+            self.env_metadata.update_dependency_hash(environment, new_dep_hash)
 
     def run_shell_commands(
         self,
