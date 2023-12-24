9aa2c70b7144e461d3f5088b4d99b84d30034f78
Justin Flannery <juftin@juftin.com>
2023-12-24 15:35:44 -0700

    fix: updated dependency hash (#1169)
    * fix: updated dependency hash
    
    * adjust tests
    
    * fix
    
    * .
    
    ---------
    
    Co-authored-by: Ofek Lev <ofekmeister@gmail.com>

--- src/hatch/cli/application.py
+++ src/hatch/cli/application.py
@@ -104,17 +104,18 @@ class Application(Terminal):
                     with self.status('Running post-installation commands'):
                         self.run_shell_commands(environment, environment.post_install_commands, source='post-install')
 
-        dep_hash = environment.dependency_hash()
+        new_dep_hash = environment.dependency_hash()
         current_dep_hash = self.env_metadata.dependency_hash(environment)
-        if dep_hash != current_dep_hash:
+        if new_dep_hash != current_dep_hash:
             with self.status('Checking dependencies'):
                 dependencies_in_sync = environment.dependencies_in_sync()
 
             if not dependencies_in_sync:
                 with self.status('Syncing dependencies'):
                     environment.sync_dependencies()
+                    new_dep_hash = environment.dependency_hash()
 
-            self.env_metadata.update_dependency_hash(environment, dep_hash)
+            self.env_metadata.update_dependency_hash(environment, new_dep_hash)
 
     def run_shell_commands(
         self,
