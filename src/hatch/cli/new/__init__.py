eeb0abc64205b7ec4491733cd0c553ca761ba400
Orkunnnn <orkunkonak1414@gmail.com>
2024-08-03 05:42:16 +0300

    feat: add email setting in interactive mode

--- src/hatch/cli/new/__init__.py
+++ src/hatch/cli/new/__init__.py
@@ -71,6 +71,7 @@ def new(app, name, location, interactive, feature_cli, initialize, setuptools_op
     default_config = {
         'description': '',
         'name': '',
+        'email': '',
         'dependencies': set(),
         'package_name': normalized_name.replace('-', '_'),
         'project_name': name,
@@ -81,7 +82,8 @@ def new(app, name, location, interactive, feature_cli, initialize, setuptools_op
     if interactive:
         default_config['description'] = app.prompt('Description', default='')
         default_config['name'] = app.prompt('Author Name', default='U.N Owen')
-        
+        default_config['email'] = app.prompt('Author Email', default='void@some.where')
+
         app.display_info()
 
     if needs_config_update:
