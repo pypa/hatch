a565a5fe9e236d2dfb602baa054c141623e5181a
Vladimir Petrigo <vladimir.petrigo@gmail.com>
2025-01-11 23:13:06 +0100

    hatch: Update to use find_uv_bin()

--- tests/helpers/helpers.py
+++ tests/helpers/helpers.py
@@ -3,6 +3,7 @@ from __future__ import annotations
 import importlib
 import json
 import os
+import pathlib
 import re
 import sys
 from datetime import datetime, timezone
@@ -50,7 +51,13 @@ def get_current_timestamp():
 
 def assert_plugin_installation(subprocess_run, dependencies: list[str], *, verbosity=0, count=1):
     uv_bin = find_uv_bin()
-    command = [uv_bin, 'pip', 'install']
+    command = [
+        uv_bin,
+        'pip',
+        'install',
+        '--directory',
+        str(pathlib.Path(sys.executable).parent.parent),
+    ]
     add_verbosity_flag(command, verbosity, adjustment=-1)
     command.extend(dependencies)
 
