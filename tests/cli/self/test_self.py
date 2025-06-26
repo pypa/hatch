df295583784427214324b974c75901da12931ec1
Daniel Garcia Moreno <daniel.garcia@suse.com>
2025-06-26 11:32:54 +0200

    Fix test_self with click >= 8.2.0
    https://click.palletsprojects.com/en/stable/changes/#version-8-2-0

--- tests/cli/self/test_self.py
+++ tests/cli/self/test_self.py
@@ -1,7 +1,13 @@
 import os
+import importlib
 
 
 def test(hatch):
     result = hatch(os.environ['PYAPP_COMMAND_NAME'])
+    exit_code = 2
 
-    assert result.exit_code == 0, result.output
+    click_version = importlib.metadata.version('click')
+    if click_version <= '8.1.8':
+        exit_code = 0
+
+    assert result.exit_code == exit_code, result.output
