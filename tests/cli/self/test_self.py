ce44bb4b4aae514445159c1c1dff6cd584fea818
Daniel Garcia Moreno <daniel.garcia@suse.com>
2025-06-27 07:57:31 +0200

    Increase minimum supported version of click to 8.2.0

--- tests/cli/self/test_self.py
+++ tests/cli/self/test_self.py
@@ -1,13 +1,7 @@
 import os
-import importlib
 
 
 def test(hatch):
     result = hatch(os.environ['PYAPP_COMMAND_NAME'])
-    exit_code = 2
 
-    click_version = importlib.metadata.version('click')
-    if click_version <= '8.1.8':
-        exit_code = 0
-
-    assert result.exit_code == exit_code, result.output
+    assert result.exit_code == 2, result.output
