585696991b6cb81cdf3eb6f853fc445aea0ed31b
Dimitri Papadopoulos <3234522+DimitriPapadopoulos@users.noreply.github.com>
2025-09-27 23:34:45 +0200

    Apply ruff/pyupgrade rule UP036
    UP036 Version block is outdated for minimum Python version

--- src/hatch/project/frontend/core.py
+++ src/hatch/project/frontend/core.py
@@ -1,7 +1,6 @@
 from __future__ import annotations
 
 import json
-import sys
 from functools import cache
 from typing import TYPE_CHECKING, Any, Literal
 
@@ -295,58 +294,33 @@ class HatchBuildFrontendScripts(BuildFrontendScripts):
         )
 
 
-if sys.version_info[:2] >= (3, 9):
+@cache
+def hook_caller_script() -> str:
+    from importlib.resources import files
 
-    @cache
-    def hook_caller_script() -> str:
-        from importlib.resources import files
+    script = files('pyproject_hooks._in_process') / '_in_process.py'
+    return script.read_text(encoding='utf-8')
 
-        script = files('pyproject_hooks._in_process') / '_in_process.py'
-        return script.read_text(encoding='utf-8')
 
-    @cache
-    def runner_script() -> str:
-        from importlib.resources import files
+@cache
+def runner_script() -> str:
+    from importlib.resources import files
 
-        script = files('hatch.project.frontend.scripts') / 'standard.py'
-        return script.read_text(encoding='utf-8')
+    script = files('hatch.project.frontend.scripts') / 'standard.py'
+    return script.read_text(encoding='utf-8')
 
-    @cache
-    def hatch_build_deps_script() -> str:
-        from importlib.resources import files
 
-        script = files('hatch.project.frontend.scripts') / 'build_deps.py'
-        return script.read_text(encoding='utf-8')
+@cache
+def hatch_build_deps_script() -> str:
+    from importlib.resources import files
 
-    @cache
-    def hatch_core_metadata_script() -> str:
-        from importlib.resources import files
+    script = files('hatch.project.frontend.scripts') / 'build_deps.py'
+    return script.read_text(encoding='utf-8')
 
-        script = files('hatch.project.frontend.scripts') / 'core_metadata.py'
-        return script.read_text(encoding='utf-8')
 
-else:
+@cache
+def hatch_core_metadata_script() -> str:
+    from importlib.resources import files
 
-    @cache
-    def hook_caller_script() -> str:
-        from importlib.resources import read_text
-
-        return read_text('pyproject_hooks._in_process', '_in_process.py')
-
-    @cache
-    def runner_script() -> str:
-        from importlib.resources import read_text
-
-        return read_text('hatch.project.frontend.scripts', 'standard.py')
-
-    @cache
-    def hatch_build_deps_script() -> str:
-        from importlib.resources import read_text
-
-        return read_text('hatch.project.frontend.scripts', 'build_deps.py')
-
-    @cache
-    def hatch_core_metadata_script() -> str:
-        from importlib.resources import read_text
-
-        return read_text('hatch.project.frontend.scripts', 'core_metadata.py')
+    script = files('hatch.project.frontend.scripts') / 'core_metadata.py'
+    return script.read_text(encoding='utf-8')
