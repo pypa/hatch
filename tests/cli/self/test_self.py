79d3c156d6d49d277d4a4014e78f84b02aac978b
Cary <hawkinscary23@gmail.com>
2025-09-27 14:07:34 -0700

    Fix failing tests, handle subprocess buffering causing output issues on terminal (#2061)
    Co-authored-by: Ofek Lev <ofekmeister@gmail.com>

--- tests/cli/self/test_self.py
+++ tests/cli/self/test_self.py
@@ -2,6 +2,6 @@ import os
 
 
 def test(hatch):
-    result = hatch(os.environ['PYAPP_COMMAND_NAME'])
+    result = hatch(os.environ['PYAPP_COMMAND_NAME'], '-h')
 
     assert result.exit_code == 0, result.output
