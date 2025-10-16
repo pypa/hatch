a14c59ec2b63f77d09ea3c4e79c6b58f31eab221
Charles-Meldhine Madi Mnemoi <63333367+cmnemoi@users.noreply.github.com>
2025-10-16 22:22:05 +0200

    fix: Fixed `Environment default is incompatible: Unknown distribution source` error (#2086)
    Co-authored-by: Ofek Lev <ofekmeister@gmail.com>

--- src/hatch/python/resolve.py
+++ src/hatch/python/resolve.py
@@ -280,7 +280,11 @@ def _get_default_variant_gil() -> str:
 
 
 def _get_distribution_class(source: str) -> type[Distribution]:
-    if source.startswith('https://github.com/astral-sh/python-build-standalone/releases/download/'):
+    if source.startswith((
+        'https://github.com/astral-sh/python-build-standalone/releases/download/',
+        # TODO: Remove compatibility for the old source
+        'https://github.com/indygreg/python-build-standalone/releases/download/',
+    )):
         return CPythonStandaloneDistribution
 
     if source.startswith('https://downloads.python.org/pypy/'):
--- tests/python/test_resolve.py
+++ tests/python/test_resolve.py
@@ -19,9 +19,12 @@ class TestErrors:
         reason='No variants for this platform and architecture combination',
     )
     def test_resolution_error(self, platform):
-        with EnvVars({'HATCH_PYTHON_VARIANT_CPU': 'foo'}), pytest.raises(
-            PythonDistributionResolutionError,
-            match=f"Could not find a default source for name='3.11' system='{platform.name}' arch=",
+        with (
+            EnvVars({'HATCH_PYTHON_VARIANT_CPU': 'foo'}),
+            pytest.raises(
+                PythonDistributionResolutionError,
+                match=f"Could not find a default source for name='3.11' system='{platform.name}' arch=",
+            ),
         ):
             get_distribution('3.11')
 
@@ -29,6 +32,14 @@ class TestErrors:
 class TestDistributionVersions:
     def test_cpython_standalone(self):
         url = 'https://github.com/astral-sh/python-build-standalone/releases/download/20230507/cpython-3.11.3%2B20230507-aarch64-unknown-linux-gnu-install_only.tar.gz'
+        dist = get_distribution('3.11', url)
+        version = dist.version
+
+        assert version.epoch == 0
+        assert version.base_version == '3.11.3'
+
+    def test_cpython_standalone_from_legacy_link(self) -> None:
+        url = 'https://github.com/indygreg/python-build-standalone/releases/download/20230507/cpython-3.11.3%2B20230507-aarch64-unknown-linux-gnu-install_only.tar.gz'
         dist = get_distribution('3.11', url)
         version = dist.version
 
@@ -151,8 +162,11 @@ class TestVariantGIL:
         assert 'freethreaded' in dist.source
 
     def test_incompatible(self, platform):
-        with EnvVars({'HATCH_PYTHON_VARIANT_GIL': 'freethreaded'}), pytest.raises(
-            PythonDistributionResolutionError,
-            match=f"Could not find a default source for name='3.12' system='{platform.name}' arch=",
+        with (
+            EnvVars({'HATCH_PYTHON_VARIANT_GIL': 'freethreaded'}),
+            pytest.raises(
+                PythonDistributionResolutionError,
+                match=f"Could not find a default source for name='3.12' system='{platform.name}' arch=",
+            ),
         ):
             get_distribution('3.12')
