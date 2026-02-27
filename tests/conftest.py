67985f0c351bfc810724dd3646f5dda6181ef231
Cary Hawkins <hcary@amazon.com>
2026-02-27 08:00:23 -0800

    Add more loops

--- tests/conftest.py
+++ tests/conftest.py
@@ -303,16 +303,14 @@ def devpi(tmp_path_factory, worker_id):
     with FileLock(lock_file):
         if not any(devpi_started_sessions.iterdir()):
             with EnvVars(env_vars):
-                subprocess.run(
-                    ["docker", "compose", "-f", compose_file, "up", "--build", "-d", "--wait"],
-                    check=False,
-                    capture_output=True,
+                subprocess.check_call(
+                    ["docker", "compose", "-f", compose_file, "up", "--build", "-d", "--wait"]
                 )
 
-            for _ in range(60):
+            for _ in range(120):
                 output = subprocess.check_output(["docker", "logs", "hatch-devpi"]).decode("utf-8")
                 if f"Serving index {dp.user}/{dp.index_name}" in output:
-                    time.sleep(30)
+                    time.sleep(5)
                     break
 
                 time.sleep(1)
