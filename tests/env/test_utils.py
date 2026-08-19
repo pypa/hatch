84e38765502088fd4d8d8a716dbfb61abb6383ac
Valentin Oliver Melberg Loftsson <23453691+valentinoli@users.noreply.github.com>
2026-08-19 10:49:33 +0000

    Refactor test parameterization for get_env_var

--- tests/env/test_utils.py
+++ tests/env/test_utils.py
@@ -4,25 +4,25 @@ from hatch.env.utils import get_env_var
 
 
 @pytest.mark.parametrize(
-  ("plugin_name", "option", "expected"),
-  [
-    ("virtual", "uv-path", "HATCH_ENV_TYPE_VIRTUAL_UV_PATH"),
-    ("virtual", "uv_path", "HATCH_ENV_TYPE_VIRTUAL_UV_PATH"),
-    ("my-plugin", "my-option", "HATCH_ENV_TYPE_MY_PLUGIN_MY_OPTION"),
-    ("MY-plugin", "Some-Option", "HATCH_ENV_TYPE_MY_PLUGIN_SOME_OPTION"),
-    ("a-b-c", "d-e-f", "HATCH_ENV_TYPE_A_B_C_D_E_F"),
-  ],
-  ids=[
-    "hyphenated-plugin",
-    "underscored-plugin",
-    "hyphenated-both",
-    "mixed-case-and-hyphens",
-    "multiple-hyphens",
-  ],
+    ("plugin_name", "option", "expected"),
+    [
+        ("virtual", "uv-path", "HATCH_ENV_TYPE_VIRTUAL_UV_PATH"),
+        ("virtual", "uv_path", "HATCH_ENV_TYPE_VIRTUAL_UV_PATH"),
+        ("my-plugin", "my-option", "HATCH_ENV_TYPE_MY_PLUGIN_MY_OPTION"),
+        ("MY-plugin", "Some-Option", "HATCH_ENV_TYPE_MY_PLUGIN_SOME_OPTION"),
+        ("a-b-c", "d-e-f", "HATCH_ENV_TYPE_A_B_C_D_E_F"),
+    ],
+    ids=[
+        "hyphenated-plugin",
+        "underscored-plugin",
+        "hyphenated-both",
+        "mixed-case-and-hyphens",
+        "multiple-hyphens",
+    ],
 )
 def test_get_env_var(
-  plugin_name: str,
-  option: str,
-  expected: str,
+    plugin_name: str,
+    option: str,
+    expected: str,
 ):
-  assert get_env_var(plugin_name, option) == expected
+    assert get_env_var(plugin_name, option) == expected
